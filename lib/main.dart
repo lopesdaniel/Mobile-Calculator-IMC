0be6fcc8bab553e5ea5bb727aca83305d980b7db
Daniel Lopes <viana.daniel02@gmail.com>
2018-10-27 23:13:03 -0300

    Terminado a tela do app

--- lib/main.dart
+++ lib/main.dart
@@ -25,26 +25,43 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white70,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person, size: 120.0, color: Colors.teal),
-          TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Peso (Kg)",
-                  labelStyle: TextStyle(color: Colors.teal)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.teal, fontSize: 25.0)),
-          TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Altura (cm)",
-                  labelStyle: TextStyle(color: Colors.teal)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.teal, fontSize: 25.0))
-        ],
-      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person, size: 120.0, color: Colors.teal),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (Kg)",
+                    labelStyle: TextStyle(color: Colors.teal)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal, fontSize: 25.0)),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.teal)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal, fontSize: 25.0)),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: (){},
+                  child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+            Text("Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal, fontSize: 25.0))
+          ],
+        ),
+      )
     );
   }
 }
